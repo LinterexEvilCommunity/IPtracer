232fcda9dc1a1c9f52a241325870ca68a1fd3558
LinterexEvilCommunity <66816937+LinterexEvilCommunity@users.noreply.github.com>
2020-06-14 14:25:49 +0530

    Update FileExporter.py

--- core/FileExporter.py
+++ core/FileExporter.py
@@ -2,29 +2,28 @@
 # encoding: UTF-8
 
 """
-    This file is part of IPtracer tool.
+    This file is part of IPGeoLocation tool
+     L
     https://github.com/LinterexEvilCommunity/IPtracer
     
-    IPtracer - Retrieve IP Geolocation information 
+    IPGeoLocation - Retrieve IP Geolocation information 
     Powered by http://ip-api.com
     
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
     
     For more see the file 'LICENSE' for copying permission.
 """
 
-__author__ = 'LinterexEvil Community'
+__author__ = 'Shellstar'
 
 import csv
 from xml.etree import ElementTree as etree
@@ -35,59 +34,59 @@ class FileExporter:
     def __init__(self):
         pass
     
-    def ExportListToCSV(self, IPtracerObjs, filename):
-        return self.__ExportToCSV(IPtracerObjs, filename)
+    def ExportListToCSV(self, ipGeoLocObjs, filename):
+        return self.__ExportToCSV(ipGeoLocObjs, filename)
         
-    def ExportToCSV(self, IPtracerObj, filename):
-        return self.__ExportToCSV([IPtracerObj], filename)
+    def ExportToCSV(self, ipGeoLocObj, filename):
+        return self.__ExportToCSV([ipGeoLocObj], filename)
     
-    def ExportListToXML(self, IPtracerObjs, filename):
-        return self.__ExportToXML(IPtracerObjs, filename)
+    def ExportListToXML(self, ipGeoLocObjs, filename):
+        return self.__ExportToXML(ipGeoLocObjs, filename)
     
-    def ExportToXML(self, IPtracerObj, filename):
-        return self.__ExportToXML([IPtracerObj], filename)
+    def ExportToXML(self, ipGeoLocObj, filename):
+        return self.__ExportToXML([ipGeoLocObj], filename)
 
-    def ExportListToTXT(self, IPtracerObjs, filename):
-        return self.__ExportToTXT(IPtracerObjs, filename)
+    def ExportListToTXT(self, ipGeoLocObjs, filename):
+        return self.__ExportToTXT(ipGeoLocObjs, filename)
         
-    def ExportToTXT(self, IPtracerObj, filename):
-        return self.__ExportToTXT([IPtracerObj], filename)
+    def ExportToTXT(self, ipGeoLocObj, filename):
+        return self.__ExportToTXT([ipGeoLocObj], filename)
     
-    def __ExportToTXT(self, IPtracerObjs, filename):
+    def __ExportToTXT(self, ipGeoLocObjs, filename):
         try:
             with open(filename, 'w') as txtfile:
-                txtfile.write('Results IPtracer\n')
-                for IPtracerObj in IPtracerObjs:
-                    if IPtracerObj:
-                        txtfile.write('Target: {}\n'.format(IPtracerObj.Query))
-                        txtfile.write('IP: {}\n'.format(IPtracerObj.IP))
-                        txtfile.write('ASN: {}\n'.format(IPtracerObj.ASN))
-                        txtfile.write('City: {}\n'.format(IPtracerObj.City))
-                        txtfile.write('Country: {}\n'.format(IPtracerObj.Country))
-                        txtfile.write('Country Code: {}\n'.format(IPtracerObj.CountryCode))
-                        txtfile.write('ISP: {}\n'.format(IPtracerObj.ISP))
-                        txtfile.write('Latitude: {}\n'.format(IPtracerObj.Latitude))
-                        txtfile.write('Longtitude: {}\n'.format(IPtracerObj.Longtitude))
-                        txtfile.write('Organization: {}\n'.format(IPtracerObj.Organization))
-                        txtfile.write('Region: {}\n'.format(IPtracerObj.Region))
-                        txtfile.write('Region Name: {}\n'.format(IPtracerObj.RegionName))
-                        txtfile.write('Timezone: {}\n'.format(IPtracerObj.Timezone))
-                        txtfile.write('Zip: {}\n'.format(IPtracerObj.Zip))
-                        txtfile.write('Google Maps: {}\n'.format(IPtracerObj.GoogleMapsLink))
+                txtfile.write('Results IPGeolocation\n')
+                for ipGeoLocObj in ipGeoLocObjs:
+                    if ipGeoLocObj:
+                        txtfile.write('Target: {}\n'.format(ipGeoLocObj.Query))
+                        txtfile.write('IP: {}\n'.format(ipGeoLocObj.IP))
+                        txtfile.write('ASN: {}\n'.format(ipGeoLocObj.ASN))
+                        txtfile.write('City: {}\n'.format(ipGeoLocObj.City))
+                        txtfile.write('Country: {}\n'.format(ipGeoLocObj.Country))
+                        txtfile.write('Country Code: {}\n'.format(ipGeoLocObj.CountryCode))
+                        txtfile.write('ISP: {}\n'.format(ipGeoLocObj.ISP))
+                        txtfile.write('Latitude: {}\n'.format(ipGeoLocObj.Latitude))
+                        txtfile.write('Longtitude: {}\n'.format(ipGeoLocObj.Longtitude))
+                        txtfile.write('Organization: {}\n'.format(ipGeoLocObj.Organization))
+                        txtfile.write('Region: {}\n'.format(ipGeoLocObj.Region))
+                        txtfile.write('Region Name: {}\n'.format(ipGeoLocObj.RegionName))
+                        txtfile.write('Timezone: {}\n'.format(ipGeoLocObj.Timezone))
+                        txtfile.write('Zip: {}\n'.format(ipGeoLocObj.Zip))
+                        txtfile.write('Google Maps: {}\n'.format(ipGeoLocObj.GoogleMapsLink))
                         txtfile.write('\n')
             return True
         except:
             return False
         
         
-    def __ExportToXML(self, IPtracerObjs, filename):
+    def __ExportToXML(self, ipGeoLocObjs, filename):
         try:
             root = etree.Element('Results')
             
-            for IPtracerObj in IPtracerObjs:
-                if IPtracerObj:
-                    orderedData = OrderedDict(sorted(IPtracerObj.ToDict().items()))
-                    self.__add_items(etree.SubElement(root, 'IPtracer'),
+            for ipGeoLocObj in ipGeoLocObjs:
+                if ipGeoLocObj:
+                    orderedData = OrderedDict(sorted(ipGeoLocObj.ToDict().items()))
+                    self.__add_items(etree.SubElement(root, 'IPGeolocation'),
                       ((key.replace(' ', ''), value) for key, value in orderedData.items()))
         
                     tree = etree.ElementTree(root)
@@ -99,28 +98,28 @@ class FileExporter:
             return False
         
         
-    def __ExportToCSV(self, IPtracerObjs, filename):
+    def __ExportToCSV(self, ipGeoLocObjs, filename):
         try:
             with open(filename, 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
-                writer.writerow(['Results', 'IPtracer'])
-                for IPtracerObj in IPtracerObjs:
-                    if IPtracerObj:
-                        writer.writerow(['Target', IPtracerObj.Query])
-                        writer.writerow(['IP', IPtracerObj.IP])
-                        writer.writerow(['ASN', IPtracerObj.ASN])
-                        writer.writerow(['City', IPtracerObj.City])
-                        writer.writerow(['Country', IPtracerObj.Country])
-                        writer.writerow(['Country Code', IPtracerObj.CountryCode])
-                        writer.writerow(['ISP', IPtracerObj.ISP])
-                        writer.writerow(['Latitude', IPtracerObj.Latitude])
-                        writer.writerow(['Longtitude', IPtracerObj.Longtitude])
-                        writer.writerow(['Organization', IPtracerObj.Organization])
-                        writer.writerow(['Region', IPtracerObj.Region])
-                        writer.writerow(['Region Name', IPtracerObj.RegionName])
-                        writer.writerow(['Timezone', IPtracerObj.Timezone])
-                        writer.writerow(['Zip', IPtracerObj.Zip])
-                        writer.writerow(['Google Maps', IPtracerObj.GoogleMapsLink])
+                writer.writerow(['Results', 'IPGeolocation'])
+                for ipGeoLocObj in ipGeoLocObjs:
+                    if ipGeoLocObj:
+                        writer.writerow(['Target', ipGeoLocObj.Query])
+                        writer.writerow(['IP', ipGeoLocObj.IP])
+                        writer.writerow(['ASN', ipGeoLocObj.ASN])
+                        writer.writerow(['City', ipGeoLocObj.City])
+                        writer.writerow(['Country', ipGeoLocObj.Country])
+                        writer.writerow(['Country Code', ipGeoLocObj.CountryCode])
+                        writer.writerow(['ISP', ipGeoLocObj.ISP])
+                        writer.writerow(['Latitude', ipGeoLocObj.Latitude])
+                        writer.writerow(['Longtitude', ipGeoLocObj.Longtitude])
+                        writer.writerow(['Organization', ipGeoLocObj.Organization])
+                        writer.writerow(['Region', ipGeoLocObj.Region])
+                        writer.writerow(['Region Name', ipGeoLocObj.RegionName])
+                        writer.writerow(['Timezone', ipGeoLocObj.Timezone])
+                        writer.writerow(['Zip', ipGeoLocObj.Zip])
+                        writer.writerow(['Google Maps', ipGeoLocObj.GoogleMapsLink])
                         writer.writerow([])
             return True
         except:
@@ -131,4 +130,3 @@ class FileExporter:
         for name, text in items:
             elem = etree.SubElement(root, name)
             elem.text = text
-
