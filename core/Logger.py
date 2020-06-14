d4476ebfa3f1a8c5400ef2c53f7720db2e39e1c3
LinterexEvilCommunity <66816937+LinterexEvilCommunity@users.noreply.github.com>
2020-06-14 14:17:01 +0530

    Update Logger.py

--- core/Logger.py
+++ core/Logger.py
@@ -24,7 +24,7 @@
     For more see the file 'LICENSE' for copying permission.
 """
 
-__author__ = 'LinterexEvil Community(Shellstar)'
+__author__ = 'LinterexEvil Community'
 
 from datetime import datetime
 import os
