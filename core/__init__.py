91f35be54bb1412386d7463824eb9f59b12c6b53
LinterexEvilCommunity <66816937+LinterexEvilCommunity@users.noreply.github.com>
2020-06-14 14:55:31 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,9 @@
 """
     This file is part of IPtracer tool.
+    C
     https://github.com/LinterexEvilCommunity/IPtracer
     
-    IPtracer - Retrieve IPtracer information 
+    IPGeoLocation - Retrieve IP Geolocation information 
     Powered by http://ip-api.com
     
     This program is free software: you can redistribute it and/or modify
@@ -21,5 +22,4 @@
     For more see the file 'LICENSE' for copying permission.
 """
 
-__author__ = 'LinterexEvil Community
-'
+__author__ = 'Shellstar'
